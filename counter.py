4defbb3b383bf8edb6141589cff8892157e0bea1
Tyler Paneitz <Tyler.Paneitz@vu.com>
2023-04-05 11:11:09 -0500

    submitted solution

--- counter.py
+++ counter.py
@@ -2,8 +2,15 @@ def countEmail():
     # This first line is provided for you
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
-        
+    senders ={}
+    with open(name) as file:
+        for line in file:
+            if line.startswith("From "):
+                line = line.strip()
+                line = line.split()
+                senders[line[1]] = senders.get(line[1], 0) + 1
+    bigsender = max(senders, key = senders.get)
+    print(bigsender, max(senders.values()) )
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
