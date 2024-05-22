04ab055861291d93904a6578552bcaba9a240cc1
Tyler Paneitz <Tyler.Paneitz@vu.com>
2024-05-22 15:26:27 -0500

    submission

--- counter.py
+++ counter.py
@@ -4,11 +4,11 @@ def countEmail():
     if len(name) < 1 : name = "mbox-short.txt"
     senders ={}
     with open(name) as file:
-        for line in file:
-            if line.startswith("From "):
-                line = line.strip()
-                line = line.split()
-                senders[line[1]] = senders.get(line[1], 0) + 1
+        for lines in file:
+            if lines.startswith("From "):
+                lines = lines.strip()
+                lines = lines.split()
+                senders[lines[1]] = senders.get(lines[1], 0) + 1
     bigsender = max(senders, key = senders.get)
     print(bigsender, max(senders.values()) )
 
